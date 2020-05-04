08246b1267ac2ca48b15051e0d8ddc2ea4b585ae
Mitch Lew <mitchginlew@gmail.com>
2020-05-03 21:24:29 -0400

    got rid of sidebar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './Toolbar.css';
 import '../SideDrawer/DrawerToggleButton';
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 const toolbar = props => (
     <header className = "toolbar">
         <nav className = "toolbar_navigation">
-            <div>
-                <DrawerToggleButton click = {props.drawerClickHandler}/>
-            </div>
             <div className = "toolbar_logo"><a href = "/">Mitch's Portfolio</a></div>
             <div className = "spacer"/>
             <div className = "toolbar_navigation-items">
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,38 +1,13 @@
 import React, {Component} from 'react';
 import Toolbar from "./Toolbar/Toolbar";
-import SideDrawer from './SideDrawer/SideDrawer';
 import "./Homepage.css";
-import Backdrop from './Backdrop/Backdrop';
 class HomePage extends Component{
-
-    state = {
-        sideDrawerOpen: false,
-      }
-    
-      drawerToggleClickHandler = () => {
-        this.setState(prevState => {
-          return { sideDrawerOpen: !prevState.sideDrawerOpen }
-        })
-      }
-    
-      backdropClickHandler = () => {
-        this.setState({ sideDrawerOpen: false })
-      }
       render() {
-        let backdrop
-        let sideDrawer
-    
-        if (this.state.sideDrawerOpen) {
-          backdrop = <Backdrop click={this.backdropClickHandler} />
-          sideDrawer = <SideDrawer />
-        }
         return (
           <div style={{ height: '100%' }}>
-            <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
-            {sideDrawer}
-            {backdrop}
+            <Toolbar/>
             <main style={{ marginTop: '64px' }}>
-              <p>This is the page content!</p>
+                <div className = "center"><img src="https://i.imgur.com/kC1LcdT.png" alt="avatar" className="avatar-img" /></div>
             </main>
           </div>
         )
