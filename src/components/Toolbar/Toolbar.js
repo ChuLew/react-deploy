3412357642de82e046ce178d3541141fa45c571e
Mitch Lew <mitchginlew@gmail.com>
2020-05-05 15:46:09 -0400

    css changes to toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,7 +3,13 @@ import './Toolbar.css';
 const toolbar = props => (
     <header className = "toolbar">
         <nav className = "toolbar_navigation">
-            <div className = "toolbar_logo"><a href = "/">Mitch's Portfolio</a></div>
+            <div className = "toolbar_navigation-items">
+                <ul>
+                    <li><a href = "#projects">Projects</a></li>
+                    <li><a href = "#about">About</a></li>
+                    <li><a href = "#contact">Contact</a></li>
+                </ul>
+            </div>
             <div className = "spacer"/>
             <div className = "toolbar_navigation-items">
                 <ul>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -9,6 +9,18 @@ class HomePage extends Component{
             <main style={{ marginTop: '64px' }}>
                 <div className = "center"><img src="https://i.imgur.com/kC1LcdT.png" alt="avatar" className="avatar-img" /></div>
             </main>
+            
+            <div id = "projects">
+            Project section
+           
+            </div>
+            <div id = "about" >
+            About Section
+            </div>
+            <div id = "contact">
+            Contact Section 
+            </div>
+
           </div>
         )
       }
