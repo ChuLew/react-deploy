e87276684c6608d78de02913fbc5a4b7e0ae94d1
Mitch Lew <mitchginlew@gmail.com>
2020-05-05 21:28:36 -0400

    type effect intro

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,19 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./Homepage.css";
-class HomePage extends Component{
-      render() {
-        return (
-          <div style={{ height: '100%' }}>
-            <div class="container">
-            <main style={{ marginTop: '58px' }}>
-                <div id ="wrapper"><img src="https://i.imgur.com/KG2V2xP.jpg?1" alt="introImage" /></div>
-            </main>
-            <div className = "centered"><h1>Hello, I'm Mitch Lew</h1></div>
-            
-            </div>
-            
+import Typewriter from 'typewriter-effect';
+
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div style={{ height: '100%' }}>
+        <div class="container">
+          <main style={{ marginTop: '58px' }}>
+            <div id="wrapper"><img src="https://i.imgur.com/KG2V2xP.jpg?1" alt="introImage" /></div>
+          </main>
+          <div className="centered">
+            <Typewriter options={{
+                skipAddStyles: false,
+                autoStart: true,
+                loop: true,
+              }}
+              className = "Typewriter"
+              onInit={(typewriter) => {
+                  typewriter.typeString('Welcome!')
+                  .pauseFor(1500)
+                  .deleteAll()
+                  typewriter.typeString('My Name is Mitch Lew')
+                  .pauseFor(2500)
+                  .deleteAll()
+                  typewriter.typeString('Disciplined')
+                  .pauseFor(100)
+                  .deleteAll()
+                  typewriter.typeString('Passionate')
+                  .pauseFor(100)
+                  .deleteAll()
+                  typewriter.typeString('Soccer Player')
+                  .pauseFor(100)
+                  .deleteAll()
+                  typewriter.typeString('Software Engineer')
+                  .pauseFor(3000)
+                  .deleteAll()
+                  typewriter.typeString('Stay tuned...')
+                  .pauseFor(1000)
+                  .deleteAll()
+                  typewriter.typeString("I'm just getting started")
+                  .pauseFor(5000)
+                  .deleteAll()
+                  .start();
+              }}
+            />
           </div>
-        )
-      }
+
+        </div>
+
+      </div>
+    )
+  }
 }
 export default HomePage;
+
+
