6dbc71b780b993b363077ddfb147d027f50bbfdb
Mitch Lew <mitchginlew@gmail.com>
2020-06-15 18:58:26 -0400

    added link to expense app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,8 @@ import "./Projects.css";
 class Projects extends Component{
       render() {
         return (
-          <div>
-             <a href="http://ec2-18-218-194-75.us-east-2.compute.amazonaws.com:8080/" rel="noopener noreferrer" target="_blank"></a>
+          <div className="center">
+             <a href="http://ec2-18-218-194-75.us-east-2.compute.amazonaws.com:8080/" rel="noopener noreferrer" target="_blank">Expense App</a>
           </div>
         )
       }
