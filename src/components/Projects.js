20ca490d3eed6af16d154063d5cbf155ca33db0e
Mitch Lew <mitchginlew@gmail.com>
2020-08-28 07:30:39 -0400

    added youtube app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,14 @@ import "./Projects.css";
 class Projects extends Component{
       render() {
         return (
-          <div className="center">
+          <div>
+             <div className="center">
              <a href="http://ec2-18-218-194-75.us-east-2.compute.amazonaws.com:8080/" rel="noopener noreferrer" target="_blank">Expense App</a>
+            <br></br>
+             <a href="https://videos-hook-av8j1ohku.vercel.app/" rel="noopener noreferrer" target="_blank">Youtube API app</a>
+          </div> 
           </div>
+         
         )
       }
 }
